96f93c5796106804bcaaf5d8a183b164f213aea0
Francisco Barrento <francisco.barrento@gmail.com>
2022-06-10 22:41:22 +0100

    feature: Adding Index and Login pages

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Welcome to Nuxtjs with FastAPI as backend with Google Authentication</h1>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>NuxtJs - FastApi</h1>
+    </div>
+</template>
